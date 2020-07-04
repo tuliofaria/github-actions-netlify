2259b49db518f40ad8e75fa662571f0bf2ae285d
Tulio Faria <tuliofaria@gmail.com>
2020-07-04 18:39:00 -0300

    fix.

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 exports.onPreInit = () => {
   if (process.argv[2] === "build") {
-    fs.rmdirSync(path.join(__dirname, "build"), { recursive: true })
+    // fs.rmdirSync(path.join(__dirname, "build"), { recursive: true })
     /*
     fs.renameSync(
       path.join(__dirname, "public"),
