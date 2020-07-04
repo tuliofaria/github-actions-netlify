9dee32acff79ed194eb89605797edf8048d7dcbf
Tulio Faria <tuliofaria@gmail.com>
2020-07-04 17:47:12 -0300

    fix build output dir.

---
+++ gatsby-node.js
@@ -0,0 +1,24 @@
+const path = require("path")
+const fs = require("fs")
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "build"), { recursive: true })
+    /*
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "build")
+    )
+    */
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "dist"))
+  /*
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+  */
+}
